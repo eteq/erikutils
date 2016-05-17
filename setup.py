8c488228acb17f45283d8e9eaa2dfc1dfc981e19
Erik Tollerud <erik.tollerud@gmail.com>
2016-05-17 17:49:20 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='erikutils',
+      version='0.1',
+      description='Collections of little utilities that are useful for me (and '
+                  'maybe others?)',
+      author='Erik Tollerud',
+      author_email='erik.tollerud@gmail.com',
+      url='https://github.com/eteq/erikutils',
+      packages=['erikutils'],
+     )
